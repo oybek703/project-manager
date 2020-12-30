da9ef6f764b338b6454ead77c1cf7cb7761ecb1b
oybek703 <hhoybek@gmail.com>
2020-12-30 15:55:37 +0500

    tables finished and merged without commits

--- src/components/ProjectManager.js
+++ src/components/ProjectManager.js
@@ -1,18 +1,42 @@
 import React, {useState} from 'react'
-import {Container, makeStyles, Typography} from "@material-ui/core"
+import {
+    Container,
+    makeStyles,
+    Paper,
+    Table,
+    TableBody,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@material-ui/core"
 import FormGroup from "@material-ui/core/FormGroup"
 import TextField from "@material-ui/core/TextField"
 import InputAdornment from "@material-ui/core/InputAdornment"
 import AddIcon from '@material-ui/icons/Add'
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Switch from "@material-ui/core/Switch"
+import TableCell from "@material-ui/core/TableCell"
 
 const useStyles = makeStyles(theme => ({
     textField: {
         maxWidth: '25em',
         margin: '1em 0'
     },
+    tpMargin: {
+        marginTop: '2em',
+        marginBottom: '1em'
+    },
+    table: {
+        marginBottom: '15em'
+    },
+    adornment: {
+        color: theme.palette.common.blue
+    }
 }))
+
+const createData = (name, date, service, feature, complexity, platforms, users, total) =>
+    ({name, date, service, feature, complexity, platforms, users, total})
 
 const ProjectManager = () => {
     const classes = useStyles()
@@ -20,12 +44,15 @@ const ProjectManager = () => {
     const [iOS, setIOS] = useState(false)
     const [android, setAndroid] = useState(false)
     const [software, setSoftware] = useState(false)
+    const [rows, setRows] = useState([
+        createData('John Doe', '30/12/2020', 'Website', 'Push Notifications','severe', 'iOS, Android', '0-10', '$1500')
+    ])
     return (
         <Container>
             <Typography variant='h4' color='primary'>Projects</Typography>
             <FormGroup>
                 <TextField className={classes.textField} placeholder='Search or create new project'
-                    InputProps={{endAdornment: <InputAdornment position='end'><AddIcon/></InputAdornment>}}/>
+                    InputProps={{endAdornment: <InputAdornment className={classes.adornment} position='end'><AddIcon/></InputAdornment>}}/>
             </FormGroup>
             <FormGroup row>
                 <FormControlLabel
@@ -45,6 +72,38 @@ const ProjectManager = () => {
                     label='Software'
                     labelPlacement='start'/>
             </FormGroup>
+            <TableContainer component={Paper} className={`${classes.tpMargin} ${classes.table}`}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Date</TableCell>
+                            <TableCell>Service</TableCell>
+                            <TableCell>Features</TableCell>
+                            <TableCell>Complexity</TableCell>
+                            <TableCell>Platforms</TableCell>
+                            <TableCell>Users</TableCell>
+                            <TableCell>Total</TableCell>
+                        </TableRow>
+                    </TableHead>
+                        <TableBody>
+                            {
+                                rows.map((row, index) => (
+                                    <TableRow key={index} >
+                                        <TableCell>{row.name}</TableCell>
+                                        <TableCell>{row.date}</TableCell>
+                                        <TableCell>{row.service}</TableCell>
+                                        <TableCell>{row.feature}</TableCell>
+                                        <TableCell>{row.complexity}</TableCell>
+                                        <TableCell>{row.platforms}</TableCell>
+                                        <TableCell>{row.users}</TableCell>
+                                        <TableCell>{row.total}</TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                        </TableBody>
+                </Table>
+            </TableContainer>
         </Container>
     )
 }
--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -1,6 +1,7 @@
 import {createMuiTheme} from "@material-ui/core"
 
 const arcBlue = '#0B72B9'
+const darkGrey = '#b8b8b8'
 
 const theme = createMuiTheme({
     palette: {
@@ -9,6 +10,9 @@ const theme = createMuiTheme({
         },
         primary: {
             main: arcBlue
+        },
+        darkText: {
+            color: darkGrey
         }
     },
     overrides: {
@@ -16,11 +20,24 @@ const theme = createMuiTheme({
             label: {
                 color: arcBlue,
                 fontWeight: '700',
-                fontSize: '1.4em'
+                fontSize: '1.4em',
+                borderColor: arcBlue
             },
             labelPlacementStart: {
                 marginLeft: 0,
                 marginRight: '2em'
+            }
+        },
+        MuiTableCell: {
+            head: {
+                color: arcBlue,
+                fontWeight: 700,
+                textAlign: 'center',
+                fontSize: '1.2em'
+            },
+            body: {
+                color: darkGrey,
+                textAlign: 'center'
             }
         }
     }
